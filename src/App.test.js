099c3d03a2421c3dac181cda49099fb650a21ef8
saleborg <Stefan.aleborg@gmail.com>
2021-11-16 17:07:14 +0100

    updated test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Site gets up', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hot Wok/i);
   expect(linkElement).toBeInTheDocument();
 });
